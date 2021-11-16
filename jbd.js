7ef5262f1a1d8db5abde87d5c50785824a644098
Sophie Wheeler <me@sophi.ee>
2021-11-16 14:08:04 +0000

    add temp parsing

--- jbd.js
+++ jbd.js
@@ -182,7 +182,7 @@ function getNTCValues(bytes, numNTCs) {
     const ntcValues = bytes.slice(27,(numNTCs*2)+27);
     let count = 0;
     let result = {};
-    for(var i = 27; i < numNTCs*2; i++) { 
+    for(var i = 27; i < 27+(numNTCs*2); i++) { 
         if(i == 27 || i % 2 != 0) {
             const ntcName = `NTC${count}`;
             result[ntcName] = toU16(bytes[i], bytes[i+1]);
