653060b5a8ea5309f16a9aaeccb63e70a7d410d1
Sophie Wheeler <me@sophi.ee>
2021-11-16 16:37:58 +0000

    use event listener properly :/

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ async function main() {
         //send requests, response handled by eventlistener
           await jbd.getRegister(0x03);
           //wait 1s between requests
-          await delay(1000);
+          await sleep(1000);
+          
           await jbd.getRegister(0x04);
       }, 
       args.pollinginterval * 1000
@@ -27,6 +28,12 @@ async function main() {
     process.exit(1);
   }
 
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
 main();
